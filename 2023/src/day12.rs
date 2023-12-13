61426d869b6d8c908d5861078eea4e742abff640
ThatXliner <bryan.hu.2020@gmail.com>
2023-12-12 21:49:25 -0800

    :zap: Multithreaded bad day 12 part 1

--- 2023/src/day12.rs
+++ 2023/src/day12.rs
@@ -1,64 +1,64 @@
+// Written entirely by ChatGPT, with minor modifications by me
+// (the logic is mine but I got ChatGPT to re-write it in Rust)
+use lazy_static::lazy_static;
+use rayon::prelude::*;
+use regex::Regex;
 use std::io::{self, BufRead};
-
+lazy_static! {
+    static ref RE: Regex = Regex::new(r"#+").unwrap();
+}
 pub fn part1() {
     let stdin = io::stdin();
-    let mut total = 0;
+    let lines: Vec<String> = stdin.lock().lines().map(|l| l.unwrap()).collect();
+    let total = lines
+        .par_iter()
+        .fold(
+            || 0,
+            |acc, line| {
+                let mut total_count = acc;
+                let mut parts = line.split(" ");
+                if let (Some(things), Some(amounts_raw)) = (parts.next(), parts.next()) {
+                    let amounts: Vec<u32> = amounts_raw
+                        .split(",")
+                        .filter_map(|x| x.parse().ok())
+                        .collect();
 
-    for (l_count, line) in stdin.lock().lines().enumerate() {
-        if let Ok(line) = line {
-            let mut parts = line.split(" ");
-            if let (Some(things), Some(amounts_raw)) = (parts.next(), parts.next()) {
-                let amounts: Vec<u32> = amounts_raw
-                    .split(",")
-                    .filter_map(|x| x.parse().ok())
-                    .collect();
+                    for i in 0..(1 << things.matches('?').count()) {
+                        let mut test = things.to_string();
 
-                for i in 0..(1 << things.matches('?').count()) {
-                    let mut test = things.to_string();
-
-                    for (j, x) in things.chars().enumerate() {
-                        if x == '?' {
-                            if i & (1 << things[0..j].matches('?').count()) != 0 {
-                                test = test.chars().enumerate().map(|(idx, c)| {
-                                    if idx == j {
-                                        '#'
-                                    } else {
-                                        c
-                                    }
-                                }).collect();
-                            } else {
-                                test = test.chars().enumerate().map(|(idx, c)| {
-                                    if idx == j {
-                                        '.'
-                                    } else {
-                                        c
-                                    }
-                                }).collect();
+                        for (j, x) in things.chars().enumerate() {
+                            if x == '?' {
+                                if i & (1 << things[0..j].matches('?').count()) != 0 {
+                                    test = test
+                                        .chars()
+                                        .enumerate()
+                                        .map(|(idx, c)| if idx == j { '#' } else { c })
+                                        .collect();
+                                } else {
+                                    test = test
+                                        .chars()
+                                        .enumerate()
+                                        .map(|(idx, c)| if idx == j { '.' } else { c })
+                                        .collect();
+                                }
                             }
                         }
-                    }
 
-                    let mut lengths = Vec::new();
-                    let re = regex::Regex::new(r"#+").unwrap();
-                    for caps in re.captures_iter(&test) {
-                        if let Some(mat) = caps.get(0) {
-                            lengths.push(mat.as_str().len() as u32);
+                        let mut lengths = Vec::new();
+                        for caps in RE.captures_iter(&test) {
+                            if let Some(mat) = caps.get(0) {
+                                lengths.push(mat.as_str().len() as u32);
+                            }
+                        }
+                        if lengths == amounts {
+                            total_count += 1;
                         }
                     }
-
-                    println!(
-                        "{} {} {} {:?} {:?}",
-                        things, i, test, lengths, amounts
-                    );
-
-                    if lengths == amounts {
-                        total += 1;
-                    }
                 }
-            }
-            println!("{} {}", l_count, total);
-        }
-    }
+                total_count
+            },
+        )
+        .sum::<usize>();
 
     println!("{}", total);
 }
